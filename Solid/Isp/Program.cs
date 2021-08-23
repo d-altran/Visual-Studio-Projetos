cea10c5f8a8e355340306ad01bb1ba14973d1248
Diego Altran <diegoaltran41@gmail.com>
2021-08-23 20:43:19 -0300

    Isp

---
+++ Solid/Isp/Program.cs
@@ -0,0 +1,20 @@
+﻿using Isp.Vehicles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Isp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car("Azul", 2022, 4.0, 2, 2);
+            Motorcycle motorcycle = new Motorcycle("Branca", 2021, 150);
+
+            Console.ReadLine();
+        }
+    }
+}
