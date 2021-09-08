6f167d377f23cc3bc54d9976bc4a567c11e69a9f
Diego Altran <diegoaltran41@gmail.com>
2021-09-08 18:02:12 -0300

    DesignPattern - Criacional

---
+++ Design Patterns/DesignPatterns/Builder/Program.cs
@@ -0,0 +1,40 @@
+﻿using Builder.Builders;
+using Builder.Directors;
+using Builder.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            VehicleBuilder builder = new VehicleBuilder();
+            Director director = new Director(builder);
+
+            director.ConstructSedanCar();
+            Vehicle sedan = builder.GetVehicle();
+            Console.WriteLine($"Criado um veículo do tipo: {sedan.VehicleType} ");
+
+            Console.ReadLine();
+
+            director.ConstructTruck();
+            Vehicle truck = builder.GetVehicle();
+            Console.WriteLine($"Criado um veículo do tipo: {truck.VehicleType} ");
+
+            Console.ReadLine();
+
+            director.ConstructSUV();
+            Vehicle suv = builder.GetVehicle();
+            Console.WriteLine($"Criado um veículo do tipo: {suv.VehicleType} ");
+
+            Console.ReadLine();
+
+        }
+    }
+}
