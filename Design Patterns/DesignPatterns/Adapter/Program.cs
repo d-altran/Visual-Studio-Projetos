fbe8fed8049e6436fb5565504bb0e0e02e6a1fbc
Diego Altran <diegoaltran41@gmail.com>
2021-09-13 15:25:09 -0300

    Adapter

---
+++ Design Patterns/DesignPatterns/Adapter/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //PayPal payment = new PayPal();
+            //Payonner payment = new Payonner();
+
+            //payment.SendPayment();
+            //payment.ReceivePayment();
+
+            IPayPalPayment payment = new PayonnerAdapter(new Payonner());
+
+            payment.PayPalPayment();
+            payment.PayPalReceive();
+
+            Console.ReadLine();
+        }
+    }
+}
