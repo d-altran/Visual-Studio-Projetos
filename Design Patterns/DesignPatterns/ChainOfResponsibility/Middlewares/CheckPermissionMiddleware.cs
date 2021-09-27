f4363f337c70020a3ccafe9d99c7f359838f8648
Diego Altran <diegoaltran41@gmail.com>
2021-09-27 19:55:36 -0300

    Chain of Responsibility

---
+++ Design Patterns/DesignPatterns/ChainOfResponsibility/Middlewares/CheckPermissionMiddleware.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsibility.Middlewares
+{
+    class CheckPermissionMiddleware : Middleware
+    {
+        public override bool Check(string email, string password)
+        {
+            if (email.Equals("master@hcode.com.br"))
+            {
+                Console.WriteLine("Seja bem-vindo administrador!");
+                return true;
+            }
+
+            Console.WriteLine("Seja bem-vindo!");
+
+            return CheckNext(email, password);
+        }
+    }
+}
